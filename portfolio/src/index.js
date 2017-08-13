0ae96d08ee37ac7ee5988be00f65daa41fde1f6b
Your Name <ian.sudderth@gmail.com>
2017-08-13 17:03:46 -0400

    pre-merge cleanup

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
-import Typography from 'material-ui/Typography'
-import Layout from './components/Layout'
+// import PropTypes from "prop-types";
+import Layout from "./components/Layout";
 
 class Portfolio extends Component {
-
-  render(){
-    return (
-		<Layout />
-      )
-  }
+	render() {
+		return <Layout />;
+	}
 }
 
-export default Portfolio
+export default Portfolio;
