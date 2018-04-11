804b4621f6e83dfa91a8ddde9f2b85e710640a4e
Your Name <ian.sudderth@gmail.com>
2018-04-10 23:19:56 -0400

    setting up docker files and configs to run in dev environment

--- server-files/config.js
+++ server-files/config.js
@@ -2,7 +2,7 @@ const config = {
   pageSize: 6,
   saltingRounds: process.env.SALTING_ROUNDS || 8,
   sessionSecret: process.env.SESSION_SECRET || 'stuff and things',
-  dburl: process.env.DATABASEURL || 'mongodb://localhost/recipe',
+  dburl: process.env.DATABASEURL || 'mongodb://mongo:27017/recipe',
   port: process.env.PORT || 3000,
   dev: process.env.NODE_ENV !== 'production',
 };
